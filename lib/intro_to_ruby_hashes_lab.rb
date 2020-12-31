efc2b9c791cc0db74ec246627ee41e9cd86d2cc2
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 07:12:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,15 +8,22 @@ def my_hash
   h = {
     :a_key, "a_value"
   }
+  return h
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  
+  h = {
+    :name, "Grace Hopper"
+  }
+  return h
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  
+  h = {
+    :id, number
+  }
+  return h
 end
